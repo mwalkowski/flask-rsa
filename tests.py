408ec2f74bb40e3737d13ff8008f577d28c9fd90
Michał Walkowski <mi.walkowski@gmail.com>
2024-01-28 18:20:19 +0100

    update tests.py

--- tests.py
+++ tests.py
@@ -1,15 +1,18 @@
-import base64
 import datetime
-import json
 import uuid
 
 import pytest
 
-from Crypto.Hash import SHA256
-from Crypto.Signature import pkcs1_15 as pkcs_signature
-
 from flask_rsa import RSA
 from flask import Flask, jsonify
+
+
+SIGNATURE_HEADER = 'X-Signature'
+NONCE_HEADER = 'X-Nonce-Value'
+NONCE_QUEUE_SIZE_LIMIT = 10
+NONCE_CREATED_AT_HEADER = 'X-Nonce-Created-At'
+TIME_DIFF_TOLERANCE_IN_SECONDS = 10.0
+PUBLIC_KEY_URL = '/public-key'
 
 
 @pytest.fixture()
@@ -44,32 +47,32 @@ def test_NONCE_HEADER_not_exists(client):
     response = client.post("/signed-body", json={})
 
     assert response.status_code == 403
-    assert response.json == {'error': F'Missing header: {RSA._NONCE_HEADER}'}
+    assert response.json == {'error': F'Missing header: {NONCE_HEADER}'}
 
 
 def test_NONCE_CREATED_AT_HEADER_not_exists(client):
-    response = client.post("/signed-body", headers={RSA._NONCE_HEADER: "a"}, json={})
+    response = client.post("/signed-body", headers={NONCE_HEADER: "a"}, json={})
 
     assert response.status_code == 403
-    assert response.json == {'error': F'Missing header: {RSA._NONCE_CREATED_AT_HEADER}'}
+    assert response.json == {'error': F'Missing header: {NONCE_CREATED_AT_HEADER}'}
 
 
 def test_SIGNATURE_HEADER_not_exists(client):
     headers = {
-        RSA._NONCE_HEADER: "a",
-        RSA._NONCE_CREATED_AT_HEADER: "b",
+        NONCE_HEADER: "a",
+        NONCE_CREATED_AT_HEADER: "b",
     }
     response = client.post("/signed-body", headers=headers, json={})
 
     assert response.status_code == 403
-    assert response.json == {'error': F'Missing header: {RSA._SIGNATURE_HEADER}'}
+    assert response.json == {'error': F'Missing header: {SIGNATURE_HEADER}'}
 
 
 def test_NONCE_HEADER_is_incorrect(client):
     headers = {
-        RSA._NONCE_HEADER: "a",
-        RSA._NONCE_CREATED_AT_HEADER: "b",
-        RSA._SIGNATURE_HEADER: "aaaa"
+        NONCE_HEADER: "a",
+        NONCE_CREATED_AT_HEADER: "b",
+        SIGNATURE_HEADER: "aaaa"
     }
     response = client.post("/signed-body", headers=headers, json={})
 
@@ -77,24 +80,11 @@ def test_NONCE_HEADER_is_incorrect(client):
     assert response.json == {'error': "Nonce is already used or incorrect"}
 
 
-def test_NONCE_HEADER_pool(client):
-    for _ in range(30):
-        headers = {
-            RSA._NONCE_HEADER: uuid.uuid4(),
-            RSA._NONCE_CREATED_AT_HEADER: "b",
-            RSA._SIGNATURE_HEADER: "aaaa"
-        }
-        response = client.post("/signed-body", headers=headers, json={})
-
-        assert response.status_code == 403
-    assert RSA._received_nonces.qsize() == 10
-
-
 def test_NONCE_HEADER_is_duplicated(client):
     headers = {
-        RSA._NONCE_HEADER: uuid.uuid4(),
-        RSA._NONCE_CREATED_AT_HEADER: "b",
-        RSA._SIGNATURE_HEADER: "aaaa"
+        NONCE_HEADER: uuid.uuid4(),
+        NONCE_CREATED_AT_HEADER: "b",
+        SIGNATURE_HEADER: "aaaa"
     }
     response = client.post("/signed-body", headers=headers, json={})
 
@@ -109,9 +99,9 @@ def test_NONCE_HEADER_is_duplicated(client):
 
 def test_NONCE_CREATED_AT_HEADER_invalid(client):
     headers = {
-        RSA._NONCE_HEADER: uuid.uuid4(),
-        RSA._NONCE_CREATED_AT_HEADER: "b",
-        RSA._SIGNATURE_HEADER: "aaaa"
+        NONCE_HEADER: uuid.uuid4(),
+        NONCE_CREATED_AT_HEADER: "b",
+        SIGNATURE_HEADER: "aaaa"
     }
     response = client.post("/signed-body", headers=headers, json={})
 
@@ -120,11 +110,11 @@ def test_NONCE_CREATED_AT_HEADER_invalid(client):
 
 def test_NONCE_CREATED_AT_HEADER_time_barred(client):
     nonce_created_at = datetime.datetime.now(datetime.timezone.utc)
-    nonce_created_at -= datetime.timedelta(days=0, seconds=RSA._TIME_DIFF_TOLERANCE_IN_SECONDS + 1)
+    nonce_created_at -= datetime.timedelta(days=0, seconds=TIME_DIFF_TOLERANCE_IN_SECONDS + 1)
     headers = {
-        RSA._NONCE_HEADER: uuid.uuid4(),
-        RSA._NONCE_CREATED_AT_HEADER: nonce_created_at.isoformat(),
-        RSA._SIGNATURE_HEADER: "aaaa"
+        NONCE_HEADER: uuid.uuid4(),
+        NONCE_CREATED_AT_HEADER: nonce_created_at.isoformat(),
+        SIGNATURE_HEADER: "aaaa"
     }
     response = client.post("/signed-body", headers=headers, json={})
 
@@ -133,9 +123,9 @@ def test_NONCE_CREATED_AT_HEADER_time_barred(client):
 
 def test_SIGNATURE_invalid(client):
     headers = {
-        RSA._NONCE_HEADER: uuid.uuid4(),
-        RSA._NONCE_CREATED_AT_HEADER: datetime.datetime.now(datetime.timezone.utc).isoformat(),
-        RSA._SIGNATURE_HEADER: "aaaa"
+        NONCE_HEADER: uuid.uuid4(),
+        NONCE_CREATED_AT_HEADER: datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        SIGNATURE_HEADER: "aaaa"
     }
     response = client.post("/signed-body", headers=headers, json={'msg': 'msg'})
 
