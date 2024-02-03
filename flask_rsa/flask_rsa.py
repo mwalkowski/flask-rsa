444c57e8ea3f217eba7853b34276708d800cb0ad
Michał Walkowski <mi.walkowski@gmail.com>
2024-02-03 14:42:01 +0100

    configuration unittests

--- flask_rsa/flask_rsa.py
+++ flask_rsa/flask_rsa.py
@@ -4,18 +4,18 @@ import queue
 import re
 import uuid
 
-from cryptography.exceptions import InvalidSignature
-from cryptography.hazmat.primitives.asymmetric import padding, rsa
-from requests import status_codes
 from datetime import datetime, timezone
 from functools import wraps
+from requests import status_codes
 
 from flask import jsonify, logging
 from flask import request
 from flask import make_response as flask_make_response
 
-
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.primitives import serialization, hashes
+
 
 _PRIVATE_KEY_PATH = 'private.key'
 _PUBLIC_KEY_PATH = 'public.pem'
@@ -29,30 +29,36 @@ _PUBLIC_KEY_URL = '/public-key'
 _UUID_PATTERN = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"
 
 
-class RSA(object):
+class RSA:
 
     def __init__(self, app=None):
-        self._received_nonces = queue.Queue()
         if app:
             self._logger = logging.create_logger(app)
+            self._received_nonces = queue.Queue()
             self.init_app(app)
 
     def init_app(self, app):
-        self._signature_header = app.config.get('RSA_SIGNATURE_HEADER', _SIGNATURE_HEADER)
-        self._nonce_header = app.config.get('RSA_NONCE_HEADER', _NONCE_HEADER)
-        self._nonce_created_at_header = app.config.get('RSA_NONCE_CREATED_AT_HEADER', _NONCE_CREATED_AT_HEADER)
-        self._nonce_queue_size_limit = app.config.get('RSA_NONCE_QUEUE_SIZE_LIMIT', _NONCE_QUEUE_SIZE_LIMIT)
-        self._time_diff_tolerance_in_seconds = app.config.get('RSA_NONCE_CREATED_AT_HEADER', _TIME_DIFF_TOLERANCE_IN_SECONDS)
-        self._error_code = app.config.get('RSA_ERROR_CODE', status_codes.codes.forbidden)
-        self._requred_headers = [self._nonce_header, self._nonce_created_at_header, self._signature_header]
+        self._signature_header = app.config.get(
+            'RSA_SIGNATURE_HEADER', _SIGNATURE_HEADER)
+        self._nonce_header = app.config.get(
+            'RSA_NONCE_HEADER', _NONCE_HEADER)
+        self._nonce_created_at_header = app.config.get(
+            'RSA_NONCE_CREATED_AT_HEADER', _NONCE_CREATED_AT_HEADER)
+        self._nonce_queue_size_limit = app.config.get(
+            'RSA_NONCE_QUEUE_SIZE_LIMIT', _NONCE_QUEUE_SIZE_LIMIT)
+        self._time_diff_tolerance_in_seconds = app.config.get(
+            'RSA_TIME_DIFF_TOLERANCE_IN_SECONDS', _TIME_DIFF_TOLERANCE_IN_SECONDS)
+        self._error_code = app.config.get(
+            'RSA_ERROR_CODE', status_codes.codes.forbidden)
 
-        server_public_path = app.config.get('RSA_PUBLIC_KEY_URL', _PUBLIC_KEY_URL)
-        app.add_url_rule(server_public_path, 'public-key', self.get_server_public_key, methods=["GET"])
+        self._requred_headers = [self._nonce_header, self._nonce_created_at_header, self._signature_header]
 
         private_key_path = app.config.get('RSA_PRIVATE_KEY_PATH', None)
         public_key_path = app.config.get('RSA_PUBLIC_KEY_PATH', None)
-
         self._prepare_server_rsa_keys(private_key_path, public_key_path)
+
+        server_public_path = app.config.get('RSA_PUBLIC_KEY_URL', _PUBLIC_KEY_URL)
+        app.add_url_rule(server_public_path, 'public-key', self.get_server_public_key, methods=["GET"])
 
     def _prepare_server_rsa_keys(self, private_key_path, public_key_path):
         if not private_key_path and not public_key_path:
@@ -114,10 +120,10 @@ class RSA(object):
                 if not self._is_nonce_created_at_correct(request):
                     return self._make_response("The request is time-barred")
 
-                if not self.is_signature_correct(request):
+                if not self._is_signature_correct(request):
                     return self._make_response("Invalid Signature")
 
-                return self.add_signature(f(*args, **kwargs), request)
+                return self._add_signature(f(*args, **kwargs), request)
 
             return decorator
         return _signature_required
@@ -128,12 +134,12 @@ class RSA(object):
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ).decode()})
 
-    def get_user_public_key(self, request):
+    def _get_user_public_key(self, request):
         return self._server_public_key
 
-    def verify(self, request, signature_input_b64, received_signature):
+    def _verify(self, request, signature_input_b64, received_signature):
         try:
-            self.get_user_public_key(request).verify(
+            self._get_user_public_key(request).verify(
                 base64.standard_b64decode(received_signature),
                 signature_input_b64,
                 padding.PSS(
@@ -146,11 +152,11 @@ class RSA(object):
             return False
         return True
 
-    def is_signature_correct(self, request):
+    def _is_signature_correct(self, request) -> bool:
         nonce_value = request.headers[self._nonce_header]
         nonce_created_at = request.headers[self._nonce_created_at_header]
         signature_input_b64 = self._create_signature_input(nonce_created_at, nonce_value, request)
-        return self.verify(request, signature_input_b64, request.headers[self._signature_header])
+        return self._verify(request, signature_input_b64, request.headers[self._signature_header])
 
     @staticmethod
     def _create_signature_input(nonce_created_at, nonce_value, request):
@@ -159,17 +165,17 @@ class RSA(object):
         signature_input_b64 = base64.standard_b64encode(signature_input.encode())
         return signature_input_b64
 
-    def add_signature(self, response, request):
+    def _add_signature(self, response, request):
         nonce = uuid.uuid4()
         nonce_created_at = datetime.now(timezone.utc).isoformat()
         signature_input_b64 = self._create_signature_input(nonce_created_at, nonce, request)
-        response.headers[self._signature_header] = self.generate_signature(signature_input_b64)
+        response.headers[self._signature_header] = self._generate_signature(signature_input_b64)
         response.headers[self._nonce_header] = nonce
         response.headers[self._nonce_created_at_header] = nonce_created_at
 
         return response
 
-    def generate_signature(self, signature_input_b64):
+    def _generate_signature(self, signature_input_b64):
         return base64.standard_b64encode(self._server_private_key.sign(
             signature_input_b64,
             padding.PSS(
@@ -188,7 +194,8 @@ class RSA(object):
     def _is_nonce_created_at_correct(self, request):
         try:
             nonce_created_at = request.headers[self._nonce_created_at_header]
-            time_diff = datetime.now().astimezone(timezone.utc) - datetime.fromisoformat(nonce_created_at)
+            time_diff = (datetime.now().astimezone(timezone.utc) -
+                         datetime.fromisoformat(nonce_created_at))
             return time_diff.total_seconds() < self._time_diff_tolerance_in_seconds
         except Exception:
             return False
@@ -206,7 +213,7 @@ class RSA(object):
             self._received_nonces.get(block=True)
 
     def _make_response(self, msg):
-        return flask_make_response(jsonify({"error": msg}), self._error_code )
+        return flask_make_response(jsonify({"error": msg}), self._error_code)
 
     @staticmethod
     def _read_private_key(filename):
--- tests/unit/test_rsa.py
+++ tests/unit/test_rsa.py
@@ -1,30 +1,45 @@
+import uuid
+from datetime import datetime, timezone, timedelta
+
 import pytest
 
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from flask_rsa import RSA
 
 
-class FlaskAppStub:
+class TestRSA(RSA):
 
-    def __init__(self):
-        self.name = 'FlaskAppStub'
-        self.debug = True
-        self.config = {}
+    def is_nonce_correct(self, request):
+        return self._is_nonce_correct(request)
 
-    def add_url_rule(self, *args, **kwargs):
-        pass
+    def get_received_nonces_queue_size(self):
+        return self._received_nonces.qsize()
+
+    def is_nonce_created_at_correct(self, request):
+        return self._is_nonce_created_at_correct(request)
+
+    def make_response(self, msg):
+        return self._make_response(msg)
+
+
+@pytest.fixture()
+def flask_mock():
+    flask_mock = MagicMock()
+    flask_mock.name = 'FlaskAppStub'
+    flask_mock.debug = True
+    flask_mock.config = {}
+    yield flask_mock
 
 
 @pytest.mark.parametrize("header", ["X-Signature", "X-Nonce-Value", "X-Nonce-Created-At"])
-def test_default_header(header):
-    stub = FlaskAppStub()
+def test_default_header(flask_mock, header):
     request_mock = MagicMock()
-    request_mock.headers.get.return_value = 'A'
+    request_mock.headers.__getitem__.return_value = 'A'
 
-    uut = RSA(stub)
-    uut.verify = MagicMock(uut.verify)
+    uut = TestRSA(flask_mock)
+    uut._verify = MagicMock(uut._verify)
 
-    uut.is_signature_correct(request_mock)
+    uut._is_signature_correct(request_mock)
 
     request_mock.headers.__getitem__.assert_any_call(header)
 
@@ -33,15 +48,118 @@ def test_default_header(header):
     ("RSA_SIGNATURE_HEADER", "X-Signature-1"),
     ("RSA_NONCE_HEADER", "X-Nonce-Value-1"),
     ("RSA_NONCE_CREATED_AT_HEADER", "X-Nonce-Created-A-1")])
-def test_header_config(config, header):
-    stub = FlaskAppStub()
+def test_header_config(flask_mock, config, header):
     request_mock = MagicMock()
-    request_mock.headers.get.return_value = 'A'
-    stub.config[config] = header
+    request_mock.headers.__getitem__.return_value = 'A'
+    flask_mock.config[config] = header
 
-    uut = RSA(stub)
-    uut.verify = MagicMock(uut.verify)
+    uut = TestRSA(flask_mock)
+    uut._verify = MagicMock(uut._verify)
 
-    uut.is_signature_correct(request_mock)
+    uut._is_signature_correct(request_mock)
 
     request_mock.headers.__getitem__.assert_any_call(header)
+
+
+def test_default_nonce_queue_limit(flask_mock):
+    request_mock = MagicMock()
+
+    uut = TestRSA(flask_mock)
+
+    for _ in range(30):
+        request_mock.headers.__getitem__.return_value = str(uuid.uuid4())
+        assert uut.is_nonce_correct(request_mock)
+
+    assert uut.get_received_nonces_queue_size() == 10
+
+
+def test_config_nonce_queue_limit(flask_mock):
+    request_mock = MagicMock()
+    flask_mock.config["RSA_NONCE_QUEUE_SIZE_LIMIT"] = 30
+
+    uut = TestRSA(flask_mock)
+
+    for _ in range(60):
+        request_mock.headers.__getitem__.return_value = str(uuid.uuid4())
+        assert uut.is_nonce_correct(request_mock)
+
+    assert uut.get_received_nonces_queue_size() == 30
+
+
+def test_default_time_diff_tolerance_in_seconds(flask_mock):
+    request_mock = MagicMock()
+    uut = TestRSA(flask_mock)
+
+    request_mock.headers.__getitem__.return_value = (
+        datetime.now().astimezone(timezone.utc).isoformat())
+
+    assert uut.is_nonce_created_at_correct(request_mock)
+
+    created_at = datetime.now().astimezone(timezone.utc)
+    created_at -= timedelta(days=0, seconds=9)
+
+    request_mock.headers.__getitem__.return_value = created_at.isoformat()
+
+    assert uut.is_nonce_created_at_correct(request_mock)
+
+    created_at = datetime.now().astimezone(timezone.utc)
+    created_at -= timedelta(days=0, seconds=10 + 1)
+
+    request_mock.headers.__getitem__.return_value = created_at.isoformat()
+
+    assert not uut.is_nonce_created_at_correct(request_mock)
+
+
+def test_config_time_diff_tolerance_in_seconds(flask_mock):
+    request_mock = MagicMock()
+    time_diff_tolerance_in_seconds = 5
+    flask_mock.config['RSA_TIME_DIFF_TOLERANCE_IN_SECONDS'] = time_diff_tolerance_in_seconds
+
+    uut = TestRSA(flask_mock)
+
+    request_mock.headers.__getitem__.return_value = (
+        datetime.now().astimezone(timezone.utc).isoformat())
+
+    assert uut.is_nonce_created_at_correct(request_mock)
+
+    created_at = datetime.now().astimezone(timezone.utc)
+    created_at -= timedelta(days=0, seconds=time_diff_tolerance_in_seconds - 1)
+
+    request_mock.headers.__getitem__.return_value = created_at.isoformat()
+
+    assert uut.is_nonce_created_at_correct(request_mock)
+
+    created_at = datetime.now().astimezone(timezone.utc)
+    created_at -= timedelta(days=0, seconds=time_diff_tolerance_in_seconds + 1)
+
+    request_mock.headers.__getitem__.return_value = created_at.isoformat()
+
+    assert not uut.is_nonce_created_at_correct(request_mock)
+
+
+@patch('flask_rsa.flask_rsa.jsonify')
+@patch('flask_rsa.flask_rsa.flask_make_response')
+def test_default_error_code(flask_make_response, jsonify, flask_mock):
+    uut = TestRSA(flask_mock)
+    jsonify.return_value = "json msg"
+
+    uut.make_response('test')
+
+    jsonify.assert_called_once_with({"error": "test"})
+
+    flask_make_response.assert_called_once_with("json msg", 403)
+
+
+@patch('flask_rsa.flask_rsa.jsonify')
+@patch('flask_rsa.flask_rsa.flask_make_response')
+def test_config_error_code(flask_make_response, jsonify, flask_mock):
+    flask_mock.config['RSA_ERROR_CODE'] = 500
+    uut = TestRSA(flask_mock)
+
+    jsonify.return_value = "json msg"
+
+    uut.make_response('test')
+
+    jsonify.assert_called_once_with({"error": "test"})
+
+    flask_make_response.assert_called_once_with("json msg", 500)
