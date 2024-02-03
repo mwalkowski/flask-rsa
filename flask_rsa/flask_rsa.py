31a878cac86477920ddc073e542400805cd0d82e
Michał Walkowski <mi.walkowski@gmail.com>
2024-02-03 15:43:16 +0100

    pylinit

--- flask_rsa/flask_rsa.py
+++ flask_rsa/flask_rsa.py
@@ -29,7 +29,7 @@ _PUBLIC_KEY_URL = '/public-key'
 _UUID_PATTERN = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"
 
 
-class RSA:
+class RSA:  # pylint: disable=too-many-instance-attributes
 
     def __init__(self, app=None):
         if app:
@@ -49,16 +49,19 @@ class RSA:
         self._time_diff_tolerance_in_seconds = app.config.get(
             'RSA_TIME_DIFF_TOLERANCE_IN_SECONDS', _TIME_DIFF_TOLERANCE_IN_SECONDS)
         self._error_code = app.config.get(
-            'RSA_ERROR_CODE', status_codes.codes.forbidden)
+            'RSA_ERROR_CODE', status_codes.codes.forbidden)  # pylint: disable=no-member
 
-        self._requred_headers = [self._nonce_header, self._nonce_created_at_header, self._signature_header]
+        self._requred_headers = [self._nonce_header,
+                                 self._nonce_created_at_header,
+                                 self._signature_header]
 
         private_key_path = app.config.get('RSA_PRIVATE_KEY_PATH', None)
         public_key_path = app.config.get('RSA_PUBLIC_KEY_PATH', None)
         self._prepare_server_rsa_keys(private_key_path, public_key_path)
 
         server_public_path = app.config.get('RSA_PUBLIC_KEY_URL', _PUBLIC_KEY_URL)
-        app.add_url_rule(server_public_path, 'public-key', self.get_server_public_key, methods=["GET"])
+        app.add_url_rule(server_public_path, 'public-key',
+                         self.get_server_public_key, methods=["GET"])
 
     def _prepare_server_rsa_keys(self, private_key_path, public_key_path):
         if not private_key_path and not public_key_path:
@@ -67,7 +70,7 @@ class RSA:
             try:
                 self._server_public_key = self._read_public_key(public_key_path)
                 self._server_private_key = self._read_private_key(private_key_path)
-            except Exception as e:
+            except Exception as e:  # pylint: disable=broad-exception-caught
                 self._logger.error(e)
                 raise e
 
@@ -84,11 +87,11 @@ class RSA:
 
             self._server_private_key = self._generate_private_key()
             self._save_private_key(private_key_path)
-            self._logger.warning(F'Private key saved in {private_key_path}')
+            self._logger.warning('Private key saved in %s', private_key_path)
 
             self._server_public_key = self._server_private_key.public_key()
             self._save_public_key(public_key_path)
-            self._logger.warning(F'Public key saved in {public_key_path}')
+            self._logger.warning('Public key saved in %s', public_key_path)
 
     def _save_public_key(self, public_key_path):
         pem = self._server_public_key.public_bytes(
@@ -148,7 +151,7 @@ class RSA:
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ).decode()})
 
-    def _get_user_public_key(self, request):
+    def _get_user_public_key(self, request):  # pylint: disable=unused-argument
         return self._server_public_key
 
     def _verify(self, request, signature_input_b64, received_signature):
@@ -174,8 +177,8 @@ class RSA:
 
     @staticmethod
     def _create_signature_input(nonce_created_at, nonce_value, request):
-        signature_input = "{}{}{}{}{}".format(request.method, request.path,
-                                              nonce_value, nonce_created_at, request.data.decode())
+        signature_input = (F"{request.method}{request.path}{nonce_value}"
+                           F"{nonce_created_at}{request.data.decode()}")
         signature_input_b64 = base64.standard_b64encode(signature_input.encode())
         return signature_input_b64
 
@@ -211,7 +214,7 @@ class RSA:
             time_diff = (datetime.now().astimezone(timezone.utc) -
                          datetime.fromisoformat(nonce_created_at))
             return time_diff.total_seconds() < self._time_diff_tolerance_in_seconds
-        except Exception:
+        except Exception:  # pylint: disable=broad-exception-caught
             return False
 
     def _is_nonce_correct(self, request):
